2c5d89039b8676fc477c286ca7c39335e7f887b9
Erhan Ertem <eertem@gmail.com>
2022-07-22 23:01:56 +0300

    Establish connection from mySQL to NodeJS

---
+++ JOIN_US_app/README.md
@@ -0,0 +1,11 @@
+# Udemy MySQL Bootcamp Steve Colt workshop
+
+# JOIN US app
+
+Installed NPM package dependencies:
+
+| Package command                        | Package link                                      | Description                                                   |
+| -------------------------------------- | ------------------------------------------------- | ------------------------------------------------------------- |
+| npm install @faker-js/faker --save-dev | https://www.npmjs.com/package/@withshepherd/faker | Create gizillion instance of fake data for our mysql database |
+| npm install mysql                      | https://www.npmjs.com/package/mysql               | A node package that bridges mySQL and NodeJS                  |
+| npm install express                    | https://www.npmjs.com/package/express             | Fast, unopinionated, minimalist web framework for node        |
--- JOIN_US_app/app.js
+++ JOIN_US_app/app.js
@@ -1,3 +1,41 @@
-for (let i = 0; i < 500; i++) {
-	console.log("Hello World!");
-}
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// faker npm package loader//////////////////////
+const { faker } = require("@faker-js/faker");
+// my sql loader/////////////////////////////////
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+
+const connection = mysql.createConnection({
+	host: "localhost",
+	user: "root", // your root username
+	password: "password", // your root user's password
+	database: "join_us", // the name of your db
+	port: "3306",
+});
+// CHECK APP CONNECTION
+// const port = process.env.PORT || 5000;
+// app.listen(port);
+// console.log("App is listening on port" + port);
+////////////////////////////////////////////////
+////////////////////////////////////////////////
+const q = "SELECT CURTIME() AS time, CURDATE() AS date, NOW() AS now";
+
+connection.query(q, function (error, results, fields) {
+	if (error) throw error;
+	console.log(results[0].time);
+	console.log(results[0].date);
+	console.log(results[0].now);
+});
+connection.end();
+
+// function generateAddress() {
+// 	console.log(faker.address.streetAddress());
+// 	console.log(faker.address.city());
+// 	console.log(faker.address.state());
+// }
+
+// generateAddress();
+// generateAddress();
+// generateAddress();
