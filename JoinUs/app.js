f2a3c0da77f4659ade250fec2167c6c7da9ab884
Erhan Ertem <eertem@gmail.com>
2022-07-23 16:24:31 +0300

    Experiment with express req-res cycles

--- JoinUs/app.js
+++ JoinUs/app.js
@@ -7,4 +7,16 @@ app.listen(8080, function () {
 
 app.get("/", (req, res) => {
 	res.send("HELLO FROM OUR WEB APP!");
-}); //Root page "/" - homepage
+}); // localhost:8080/
+
+app.get("/joke", (req, res) => {
+	const joke =
+		"What do you call a dog that does magic trocks? A labracadabrador.";
+	console.log("REQUESTED THE JOKE ROUTE!");
+	res.send(joke);
+}); // localhost:8080/joke
+
+app.get("/random_num", (req, res) => {
+	const num = Math.floor(Math.random() * 10) + 1;
+	res.send("Your lucky number is " + num);
+}); // localhost:8080/random_num
