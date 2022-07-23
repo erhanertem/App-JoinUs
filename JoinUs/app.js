f8dc801ac3404d16a07fb0c51bfbda49109121a5
Erhan Ertem <eertem@gmail.com>
2022-07-23 16:29:46 +0300

    Add mysql package and establish connection with the join_us db

--- JoinUs/app.js
+++ JoinUs/app.js
@@ -1,9 +1,18 @@
-const express = require("express"); // Add express library package
+const mysql = require("mysql"); // Add mysql package
+const express = require("express"); // Add express package
 const app = express(); //Execute the express package
 
 app.listen(8080, function () {
 	console.log("App listening on port 8080!");
 }); //Set express to listen at port 8080 defaulting to localhost:8080 for a browser
+
+const connection = mysql.createConnection({
+	host: "localhost",
+	user: "root", // your root username
+	password: "password", // your root user's password
+	database: "join_us", // the name of your db
+	port: "3306", // mySQL db port
+}); //Set connection with mysql database called join_us @ port 3306
 
 app.get("/", (req, res) => {
 	res.send("HELLO FROM OUR WEB APP!");
