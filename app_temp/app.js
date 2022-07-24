9325bede6ab90c99420ea7bddc6abc5ee2364107
Erhan Ertem <eertem@gmail.com>
2022-07-24 09:59:55 +0300

    REvisit old sql npm lectures and add some note

--- app_temp/app.js
+++ app_temp/app.js
@@ -20,28 +20,29 @@ const connection = mysql.createConnection({
 // console.log("App is listening on port" + port);
 ////////////////////////////////////////////////
 ////////////////////////////////////////////////
+
 // // SELECTING DATA
 // const q = "SELECT * FROM users";
-
 // connection.query(q, function (error, results, fields) {
 // 	if (error) throw error;
 // 	console.log(results);
 // });
 // connection.end();
+
 // // INSERTING DATA
 // const q = 'INSERT INTO users (email) VALUES ("rusty_the_dog@yahoo.com")';
-
 // connection.query(q, function (error, results, fields) {
 // 	if (error) throw error;
 // 	console.log(results);
 // });
 // connection.end();
+
 // // INSERTING DATA GEN 2 - SINGLE INPUT
 // const person = {
 // 	email: faker.internet.email(),
 // 	created_at: faker.date.past(),
 // };
-// // NOTE: Behind the scene mysql node actually interprets it as 'INSERT INTO users (email) VALUES ("Jenny467@gmail.com")`
+// // NOTE: Behind the scene mysql node actually interprets it as 'INSERT INTO users (email) VALUES ("Jenny467@gmail.com")` In this example person is a JS object. Since the SQL querry phrase is "INSERT INTO users SET ?", it would require an object input so that it could acquire (email, created_at) and (faker.internet.email(),faker.date.past()) data couples internally.
 // const end_result = connection.query(
 // 	"INSERT INTO users SET ?",
 // 	// NOTE: for ? syntax, refer to mysql node manuel for further information
@@ -52,17 +53,15 @@ const connection = mysql.createConnection({
 // 	},
 // );
 // connection.end();
-
 // console.log(end_result);
 // Solicit the information sendby the mysql node to mysql
 
 // // INSERTING DATA GEN 2 - MULTIPLE INPUT
-// // mysql node documentation asks for an array for multiple inputs
+// mysql node documentation asks for an array for multiple inputs
 // const data = [];
 // for (let i = 0; i < 500; i++) {
 // 	data.push([faker.internet.email(), faker.date.past()]);
 // }
-
 // console.log(data);
 // const q = "INSERT INTO users (email, created_at) VALUES ?";
 
@@ -71,6 +70,7 @@ const connection = mysql.createConnection({
 // 	console.log(result);
 // });
 // connection.end();
+// NOTE: mysql npm manual tallks about deep destructuring nested arrays into grouped lists.  So when  SQL querry phrase is "INSERT INTO users (email, created_at) VALUES ?" where (email, created_at) is given already inside it only the data couples are required. So, [data] extracts the data as [...],[...],[...] and per documentation these are turned into ("x","x"), ("x","x"),....
 
 // // Coding Challenge: LECTURE 299 QUIZ
 // //1st Challange
