ac364fb1d4defe508fce16e3e9a67c770e731f11
Erhan Ertem <eertem@gmail.com>
2022-07-24 16:48:43 +0300

    Complete session 18 - More stuff related to managing triggers

--- triggers/ig.sql
+++ triggers/ig.sql
@@ -1,3 +1,4 @@
+-- -- NOTE: DWELLS ON WORK @ INSTAGRAM FOLDER. THIS IS JUST AN ITERATION ON THE WORK RELATED TO TRIGGER EXERCISES
 -- SHOW DATABASES;
 -- USE ig_clone;
 -- SHOW TABLES;
@@ -6,3 +7,23 @@
 -- SOURCE triggers/ig_trigger.sql;
 -- INSERT INTO follows(follower_id,followee_id) VALUES(4,4);
 -- NOTE: a follower could not follow himself due to established trigger @ ig_trigger.sql
+-- -- CREATE unfollows TABLE TO REGISTER THE UNFOLLOW EVENTS
+-- CREATE TABLE unfollows (
+--   follower_id INT NOT NULL,
+--   followee_id INT NOT NULL,
+--   created_at TIMESTAMP DEFAULT NOW(),
+--   FOREIGN KEY(follower_id) REFERENCES users(id),
+--   FOREIGN KEY(followee_id) REFERENCES users(id),
+--   PRIMARY KEY(follower_id,followee_id)
+-- );
+
+-- DELETE
+-- FROM follows
+-- WHERE follower_id = 2 && followee_id = 1;
+
+-- SELECT  *
+-- FROM unfollows;
+-- -- unfollows receives the deleted follow. 
+
+-- DROP TRIGGER character_set_client;
+-- -- removes the existing trigger
